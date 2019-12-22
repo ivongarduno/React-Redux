d453b11d0ceaee48742323f9705902edef4d41ed
BLANK-TECH <ivonne_1412@hotmail.com>
2019-12-21 21:05:10 -0600

    Updates

---
+++ precache-manifest.479635b6b6bdca2d091776b74ac57721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec2f289eb6434e1fbe1e83b4f1f2b49",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "0ddb9a4b89d308d38287",
+    "url": "/React-Redux/static/js/2.607ea709.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-Redux/static/js/2.607ea709.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc6fbc0487bbc11d0b9b",
+    "url": "/React-Redux/static/js/main.78ebf869.chunk.js"
+  },
+  {
+    "revision": "f67b8011e6a6a192d835",
+    "url": "/React-Redux/static/js/runtime-main.5761b9b7.js"
+  }
+]);
